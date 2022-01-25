d9e91c36ec827e54b5ef9d97cda1e16e694393c2
Sudeep <sdpjswl1@gmail.com>
2022-01-25 16:36:02 +0530

    Changed indentation level to 4-4

--- Example/ASJExpandableTextViewExample/ViewController.m
+++ Example/ASJExpandableTextViewExample/ViewController.m
@@ -11,7 +11,7 @@
 
 @interface ViewController () <UITextViewDelegate>
 {
-  IBOutlet ASJExpandableTextView *textView;
+    IBOutlet ASJExpandableTextView *textView;
 }
 
 - (void)setup;
@@ -23,28 +23,28 @@
 
 - (void)viewDidLoad
 {
-  [super viewDidLoad];
-  [self setup];
+    [super viewDidLoad];
+    [self setup];
 }
 
 #pragma mark - Setup
 
 - (void)setup
 {
-  textView.placeholder = @"Type something here...";
-  textView.isExpandable = YES;
-  textView.maximumLineCount = 14;
-  textView.lineSpacing = 1.5f;
-  textView.shouldShowDoneButtonOverKeyboard = YES;
-  [textView setDoneTappedBlock:^(NSString * _Nonnull text)
-   {
-     NSLog(@"you typed: %@", text);
-   }];
+    textView.placeholder = @"Type something here...";
+    textView.isExpandable = YES;
+    textView.maximumLineCount = 14;
+    textView.lineSpacing = 1.5f;
+    textView.shouldShowDoneButtonOverKeyboard = YES;
+    [textView setDoneTappedBlock:^(NSString * _Nonnull text)
+     {
+        NSLog(@"you typed: %@", text);
+    }];
 }
 
 - (IBAction)copyTapped:(id)sender
 {
-  [UIPasteboard generalPasteboard].string = @"'Cause this music can put a human being in a trance like state and deprive it for the sneaking feeling of existing. 'Cause music is bigger than words and wider than pictures. If someone said that Mogwai are the stars I would not object. If the stars had a sound it would sound like this. The punishment for these solemn words can be hard. Can blood boil like this at the sound of a noisy tape that I've heard. I know one thing. On Saturday, the sky will crumble together (or something) with a huge bang to fit into the cave.";
+    [UIPasteboard generalPasteboard].string = @"'Cause this music can put a human being in a trance like state and deprive it for the sneaking feeling of existing. 'Cause music is bigger than words and wider than pictures. If someone said that Mogwai are the stars I would not object. If the stars had a sound it would sound like this. The punishment for these solemn words can be hard. Can blood boil like this at the sound of a noisy tape that I've heard. I know one thing. On Saturday, the sky will crumble together (or something) with a huge bang to fit into the cave.";
 }
 
 @end
